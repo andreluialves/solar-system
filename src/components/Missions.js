dfc4213e6f36defa46b0e2a1aec0bd1c4a7ec676
André Alves <andreluialves@hotmail.com>
2022-02-19 00:15:27 -0300

    feat: adicionada a renderização das missões - requisito 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Title from './Title';
 import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <>
         <div data-testid="missions"><Title headline="Missões" /></div>
-        <MissionCard />
+        { missions.map((item) => (<MissionCard
+          key={ item.name }
+          name={ item.name }
+          year={ item.year }
+          country={ item.country }
+          destination={ item.destination }
+        />))}
       </>
     );
   }
