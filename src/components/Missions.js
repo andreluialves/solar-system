2f23dc635fc328b1c2b53a2f76365bd5ac76e2d7
André Alves <andreluialves@hotmail.com>
2022-02-18 23:51:57 -0300

    feat: adicionado o componente de estruturação de cada missão - requisito 9

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions"><Title headline="Missões" /></div>
+      <>
+        <div data-testid="missions"><Title headline="Missões" /></div>
+        <MissionCard />
+      </>
     );
   }
 }
